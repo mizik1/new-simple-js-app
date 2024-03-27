a39d424de31c7a01064e05854e2a66abf503ae75
mizik1 <mattk29@gmail.com>
2024-03-27 15:12:25 -0500

    Created pokemonList
    An array with 3 objects: name, height, type

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  { name: "Bulbasur", height: 7, types: ["grass", "poison"] },
+  { name: "Charizard", height: 1, types: ["fire", "flying"] },
+  { name: "Squirtle", height: 5, types: ["water"] },
+];
