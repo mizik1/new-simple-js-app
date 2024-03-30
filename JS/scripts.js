7cd31cf87e985f5446b4df587708441fac60102f
mizik1 <mattk29@gmail.com>
2024-03-30 12:31:22 -0500

    Added for loop with height conditions
    Also added line breaks and font styling to CSS body

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
-  { name: "Bulbasur", height: 7, types: ["grass", "poison"] },
-  { name: "Charizard", height: 1, types: ["fire", "flying"] },
-  { name: "Squirtle", height: 5, types: ["water"] },
+  { name: "Bulbasur", height: 2.04, types: ["grass", "poison"] },
+  { name: "Charizard", height: 5.07, types: ["fire", "flying"] },
+  { name: "Squirtle", height: 1.08, types: ["water"] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 1.5) {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " - Wow! This is a short pokemon! ");
+  } else if (pokemonList[i].height > 1.5 && pokemonList[i].height < 4.5) {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " - Wow! This is a medium pokemon! " + "<br>");
+  } else {
+    document.write(pokemonList[i].name + " height: " + pokemonList[i].height + " - Wow! This is a tall pokemon!" + "<br>");
+  }
+}
