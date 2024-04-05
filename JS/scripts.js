c4248ced96967bee27ac2e9c81e9f1458d67d844
mizik1 <mattk29@gmail.com>
2024-04-05 10:08:20 -0500

    Wrapped pokemonList in IIFE
    Added getAll function to return private data from pokemonRepository
    
    Added return to expose getAll data

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,26 +1,28 @@
-// Pokemon data
-let pokemonList = (function () {
-  return [
+// Pokemon data is private in this method by wrapping in IIFE
+let pokemonRepository = (function () {
+  // Pokemon data array
+  let pokemonList = [
     { name: "Bulbasur", height: 2.04, types: ["grass", "poison"] },
     { name: "Charizard", height: 5.07, types: ["fire", "flying"] },
     { name: "Squirtle", height: 1.08, types: ["water"] },
   ];
+
+  // Returns pokemonList
+  function getAll() {
+    return pokemonList;
+  }
+
+  // Exposes private data in IIFE by returing getAll
+  return {
+    getAll,
+  };
 })();
-
-// Pokemon repository to hold return from IIFE
-let pokemonRepository = function () {
-  let pokemonList = [];
-};
-
-function getAll() {
-  return pokemonList;
-}
 
 // container for HTML
 const container = document.body;
 
-// Categorizes pokemons by height
-pokemonList.forEach(function (pokemon) {
+// Filters pokemons by height. For Loop pulls from pokemonRepository (pokemon)
+pokemonRepository.getAll().forEach(function (pokemon) {
   if (pokemon.height < 1.5) {
     document.write(
       '<div class="pokemon-info">' + pokemon.name + " height: " + pokemon.height + " - Wow! This is a short pokemon! "
